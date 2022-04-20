8764031cd639e70a051e711c88d54ebcfe79e9f1
dannyfrelink <danny-frelink@hotmail.com>
2022-04-20 12:18:15 +0200

    create base files app

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 5151;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render();
+});
+app.get('/detail', (req, res) => {
+    res.render();
+});
+
+app.listen(PORT, () => {
+    console.log(`Application started on port: http://localhost:${PORT}`);
+});
