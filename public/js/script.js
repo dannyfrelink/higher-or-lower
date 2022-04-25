270c59c32d67068e56991269453c814f1326cc50
dannyfrelink <danny-frelink@hotmail.com>
2022-04-25 15:51:28 +0200

    remove old code

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,39 @@
 const socket = io();
-const chatForm = document.getElementById('chat');
-const chatMessage = document.getElementById('chat-message');
+// const chatForm = document.getElementById('chat');
+// const chatMessage = document.getElementById('chat-message');
 
-if (window.location.pathname === '/chat') {
-    const username = new URLSearchParams(window.location.search).get('nickname')
+if (window.location.pathname === '/game') {
+    // const username = new URLSearchParams(window.location.search).get('nickname')
 
-    chatForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        if (chatMessage.value) {
-            socket.emit('chat-message', {
-                message: chatMessage.value,
-                nickname: username
-            });
-            chatMessage.value = '';
-        }
-    });
+    // chatForm.addEventListener('submit', (event) => {
+    //     event.preventDefault();
+    //     if (chatMessage.value) {
+    //         socket.emit('chat-message', {
+    //             message: chatMessage.value,
+    //             nickname: username
+    //         });
+    //         chatMessage.value = '';
+    //     }
+    // });
 
-    socket.on('chat-message', (msg) => {
-        const item = document.createElement('li');
-        item.textContent = `${msg.nickname}: ${msg.message}`;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
-    
-    socket.on('connected', (msg) => {
-        const item = document.createElement('li');
-        item.textContent = msg;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
-    
-    socket.on('disconnected', (msg) => {
-        const item = document.createElement('li');
-        item.textContent = msg;
-        messages.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-    });
+    // socket.on('chat-message', (msg) => {
+    //     const item = document.createElement('li');
+    //     item.textContent = `${msg.nickname}: ${msg.message}`;
+    //     messages.appendChild(item);
+    //     window.scrollTo(0, document.body.scrollHeight);
+    // });
+
+    // socket.on('connected', (msg) => {
+    //     const item = document.createElement('li');
+    //     item.textContent = msg;
+    //     messages.appendChild(item);
+    //     window.scrollTo(0, document.body.scrollHeight);
+    // });
+
+    // socket.on('disconnected', (msg) => {
+    //     const item = document.createElement('li');
+    //     item.textContent = msg;
+    //     messages.appendChild(item);
+    //     window.scrollTo(0, document.body.scrollHeight);
+    // });
 }
